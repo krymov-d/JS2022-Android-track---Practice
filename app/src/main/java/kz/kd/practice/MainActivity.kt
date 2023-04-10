61b9f58fd6b4cbe088ec3be4f888f4bf4f1dfcc4
krymov.dulat <krymov.dulat@gmail.com>
2023-04-10 16:18:36 +0600

    Added simple notification

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -1,11 +1,64 @@
 package kz.kd.practice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlin.random.Random
+
+private const val CHANNEL_ID = "1234"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShow: Button = findViewById(R.id.btn_show_notification)
+        btnShow.setOnClickListener {
+            showNotification()
+        }
+        createNotificationChannel()
+    }
+
+    private fun showNotification() {
+        val intent = Intent(this, NotificationActivity::class.java)
+//        val intent = Intent(this, NotificationActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_custom_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello There!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationID = Random.nextInt()
+        NotificationManagerCompat.from(this).notify(notificationID, builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        val name = "Lesson 27 Channel"
+        val descriptionText = "This is our first channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
