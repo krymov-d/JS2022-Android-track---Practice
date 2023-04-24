5596692130947c85ed00b049f76bb1011e6b798b
krymov.dulat <krymov.dulat@gmail.com>
2023-04-24 17:58:55 +0600

    Fragment lifecycle

--- app/src/main/java/kz/kd/practice/MainFragment.kt
+++ app/src/main/java/kz/kd/practice/MainFragment.kt
@@ -1,5 +1,62 @@
 package kz.kd.practice
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class MainFragment : Fragment(R.layout.fragment_main)
+private const val TAG = "FragmentLife"
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "MainFragment onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "MainFragment onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "MainFragment onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "MainFragment onViewCreated")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "MainFragment onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "MainFragment onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "MainFragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "MainFragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "MainFragment onDetach")
+    }
+}
--- app/src/main/java/kz/kd/practice/SecondFragment.kt
+++ app/src/main/java/kz/kd/practice/SecondFragment.kt
@@ -1,5 +1,62 @@
 package kz.kd.practice
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class SecondFragment : Fragment(R.layout.fragment_second)
+private const val TAG = "FragmentLife"
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "SecondFragment onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "SecondFragment onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "SecondFragment onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "SecondFragment onViewCreated")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "SecondFragment onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "SecondFragment onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "SecondFragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "SecondFragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "SecondFragment onDetach")
+    }
+}
