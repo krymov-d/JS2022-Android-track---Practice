ba4fa3c44a493920af2ceb6d3d64703e3d803319
krymov.dulat <krymov.dulat@gmail.com>
2023-04-24 17:40:04 +0600

    Add and replace transaction with back stack

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -2,6 +2,7 @@ package kz.kd.practice
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,16 +10,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainer, MainFragment(), "MainFragment")
-                .commit()
+        var addToBackStack = false
+        findViewById<CheckBox>(R.id.checkBox).setOnCheckedChangeListener { _, isChecked ->
+            addToBackStack = isChecked
         }
-//      Методы транзакции
-//          add() - Добавляет фрагмент к активити
-//          remove() - Удаляет фрагмент из активити
-//          replace() - Заменяет сущефрагмент
-//          hide() - Прячет фрагмент (делает невидимым на экране)
-//          show() - Выводит скрытый фрагмент на экран
+
+        findViewById<Button>(R.id.add).setOnClickListener {
+            val transaction = supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, MainFragment(), "MainFragment")
+
+            if (addToBackStack) transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
+
+        findViewById<Button>(R.id.replace).setOnClickListener {
+            val transaction = supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, SecondFragment(), "SecondFragment")
+
+            if (addToBackStack) transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
     }
 }
---
+++ app/src/main/java/kz/kd/practice/SecondFragment.kt
@@ -0,0 +1,5 @@
+package kz.kd.practice
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second)
