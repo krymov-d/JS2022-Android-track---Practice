1c5d539a07d686970c386cd47605e6ad806ee882
krymov.dulat <krymov.dulat@gmail.com>
2023-05-01 19:18:12 +0600

    RecyclerView customization

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -3,8 +3,15 @@ package kz.kd.practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "RecyclerView_II"
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,23 +39,62 @@ class MainActivity : AppCompatActivity() {
             layoutManager = animalManager
         }
 
-        val animalList = listOf(
-            Cat("Lola", R.drawable.ic_launcher_background),
-            Dog("Tapash", R.drawable.ic_launcher_foreground),
-            Cat("Lola 2", R.drawable.ic_launcher_background),
-            Dog("Tapash 2", R.drawable.ic_launcher_foreground),
-            Cat("Lola 3", R.drawable.ic_launcher_background),
-            Dog("Tapash 3", R.drawable.ic_launcher_foreground),
-            Dog("Tapash", R.drawable.ic_launcher_foreground),
-            Dog("Tapash 2", R.drawable.ic_launcher_foreground),
-            Cat("Lola", R.drawable.ic_launcher_background),
-            Cat("Lola", R.drawable.ic_launcher_background),
-            Cat("Lola", R.drawable.ic_launcher_background),
-            Cat("Lola", R.drawable.ic_launcher_background),
-            Dog("Tapash 3", R.drawable.ic_launcher_foreground)
-        )
+        val animalList = mutableListOf<Animal>()
+        for (i in 0..100) {
+            val cat = Cat("Murka $i", R.drawable.ic_sun)
+            animalList.add(cat)
+            val dog = Dog("Mukhtar $i", R.drawable.ic_moon)
+            animalList.add(dog)
+        }
 
         animalAdapter.setItems(animalList)
+
+        //LayoutManager
+        var position: Int = 0
+        animalManager.scrollToPosition(40)
+        position = animalManager.findFirstVisibleItemPosition()
+        Log.d(TAG, position.toString())
+        position = animalManager.findFirstCompletelyVisibleItemPosition()
+        Log.d(TAG, position.toString())
+        position = animalManager.findLastVisibleItemPosition()
+        Log.d(TAG, position.toString())
+        position = animalManager.findLastCompletelyVisibleItemPosition()
+        Log.d(TAG, position.toString())
+
+
+        //ItemAnimator
+        rvMain.itemAnimator = DefaultItemAnimator() // по умолчанию (можно не писать)
+        rvMain.itemAnimator = null // отключаем анимацию
+
+
+        //OnScrollListener
+        val scrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                Log.d(TAG, newState.toString())
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                Log.d(TAG, "dx = $dx, dy = $dy")
+            }
+        }
+        rvMain.addOnScrollListener(scrollListener)
+
+        //SmoothScroller
+        val smoothScroller = object : LinearSmoothScroller(this) {
+            override fun getVerticalSnapPreference(): Int {
+                return SNAP_TO_END
+            }
+        }
+        smoothScroller.targetPosition = 90
+        animalManager.startSmoothScroll(smoothScroller)
+
+        //ItemDecoration
+        val dividerItemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        rvMain.addItemDecoration(dividerItemDecoration)
+
+        //Scrollbars
+        rvMain.isVerticalScrollBarEnabled = true
+        rvMain.isHorizontalScrollBarEnabled = true
     }
 
     private fun setupSimpleAdapter() {
