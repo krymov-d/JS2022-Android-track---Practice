53981fdadb3222135c3443ed91c4803c08bb3e37
krymov.dulat <krymov.dulat@gmail.com>
2023-05-04 18:04:12 +0600

    Menu

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -3,7 +3,11 @@ package kz.kd.practice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +22,26 @@ class MainActivity : AppCompatActivity() {
             Intent(this, SecondActivity::class.java).also {
                 startActivity(it)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflaterMain: MenuInflater = menuInflater
+        menuInflaterMain.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_close -> {
+                Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menu_more -> {
+                Toast.makeText(this, "More", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
