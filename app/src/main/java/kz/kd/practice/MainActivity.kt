83be030ff86016f5559311b3e250c7cc51ddeb97
krymov.dulat <krymov.dulat@gmail.com>
2023-05-04 18:32:10 +0600

    Context menu

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -1,27 +1,75 @@
 package kz.kd.practice
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnMain: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
+        initViews()
+        initSecondActivity()
+        initContextMenu()
+
+    }
+
+    private fun initToolbar() {
         val tbMain: Toolbar = findViewById(R.id.tb_main)
         setSupportActionBar(tbMain)
+    }
 
-        val btnMain: Button = findViewById(R.id.btn_main)
+    private fun initViews() {
+        btnMain = findViewById(R.id.btn_main)
+    }
+
+    private fun initSecondActivity() {
         btnMain.setOnClickListener {
             Intent(this, SecondActivity::class.java).also {
                 startActivity(it)
             }
+        }
+    }
+
+    private fun initContextMenu() {
+        registerForContextMenu(btnMain)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_close -> {
+                Toast.makeText(this, "Context close", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.menu_more -> {
+                Toast.makeText(this, "Context more", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> return super.onContextItemSelected(item)
         }
     }
 
@@ -37,10 +85,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show()
                 true
             }
+
             R.id.menu_more -> {
                 Toast.makeText(this, "More", Toast.LENGTH_SHORT).show()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
