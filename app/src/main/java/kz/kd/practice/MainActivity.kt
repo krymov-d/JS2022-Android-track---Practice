902290f62f083c506d9362da19bd7d72ff646d0c
krymov.dulat <krymov.dulat@gmail.com>
2023-05-05 18:57:54 +0600

    Alert dialog with list

--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -2,14 +2,11 @@ package kz.kd.practice
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
-import android.app.ProgressDialog.show
 import android.app.TimePickerDialog
-import android.content.DialogInterface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 
@@ -18,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnDatePicker: Button
     private lateinit var btnTimePicker: Button
     private lateinit var btnSimpleAlertDialog: Button
+    private lateinit var btnAlertDialogWithList: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +27,8 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         btnDatePicker = findViewById(R.id.btn_date_picker)
         btnTimePicker = findViewById(R.id.btn_time_picker)
-        btnSimpleAlertDialog = findViewById(R.id.btn_simpleAlertDialog)
+        btnSimpleAlertDialog = findViewById(R.id.btn_alert_dialog_simple)
+        btnAlertDialogWithList = findViewById(R.id.btn_alert_dialog_with_list)
     }
 
     private fun initClickListeners() {
@@ -78,6 +77,29 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "$which is pressed", Toast.LENGTH_SHORT).show()
                 }
                 .show()
+        }
+
+        btnAlertDialogWithList.setOnClickListener {
+            val students = arrayOf(
+                "Student A",
+                "Student B",
+                "Student C",
+                "Student D",
+                "Student E",
+                "Student F",
+                "Student G",
+                "Student H"
+            )
+            AlertDialog.Builder(this).apply {
+                setTitle("List of students")
+                setItems(students) { _, itemIndex ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "${students[itemIndex]} is pressed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }.show()
         }
     }
 }
