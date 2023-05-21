284cfe2d84e663a1807864ba40b020bf2e3aff27
krymov.dulat <krymov.dulat@gmail.com>
2023-05-21 15:41:11 +0600

    Empty bottom sheet

---
+++ app/src/main/java/kz/kd/practice/BottomSheetDialog.kt
@@ -0,0 +1,18 @@
+package kz.kd.practice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetDialog : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_bottom_sheet, container, false)
+    }
+}
--- app/src/main/java/kz/kd/practice/MainActivity.kt
+++ app/src/main/java/kz/kd/practice/MainActivity.kt
@@ -1,11 +1,16 @@
 package kz.kd.practice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.button).setOnClickListener {
+            BottomSheetDialog().show(supportFragmentManager, null)
+        }
     }
 }
